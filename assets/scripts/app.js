37d93f1026ae8f1e6da943fcfae310fc1f69b95c
Ravrinder Singh <ravrinder4@gmail.com>
2024-10-20 23:32:06 +0530

    tic tac toe

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,8 +4,30 @@ const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const player1Name = urlParams.get('player-1');
 const player2Name = urlParams.get('player-2');
+const player1Score = document.querySelector('.player-1-score');
+const player2Score = document.querySelector('.player-2-score');
+// const theme = document.getElementById('theme');
+
+// theme.addEventListener('change', (value) => {
+//     console.log(value);
+//     document.getElementById('app').style.backgroundColor = theme.value;
+//     document.getElementById('app').style.color = "#fff";
+
+// });
+let winningRecord = {
+    [player1Name]: 0,
+    [player2Name]: 0,
+}
 
 window.addEventListener(`DOMContentLoaded`, generateCells);
+
+function updateScores() {
+    player1Score.innerHTML = player1Name + `: <span>${winningRecord[player1Name]}</span>`;
+    player2Score.innerHTML = player2Name + `: <span>${winningRecord[player2Name]}</span>`;
+    
+}
+
+updateScores();
 
 function generateCells () {
     for(let idx = 0; idx < 9; idx++) {
@@ -49,22 +71,40 @@ app.addEventListener(`click`, (event)=> {
         }
     }
     });
-
+let flag;
     function checkCase(idx1, idx2, idx3) {
+        
         let cells = document.querySelectorAll(`#app .cell`);
         if(cells[idx1].textContent === 'X' && cells[idx2].textContent === 'X' && cells[idx3].textContent === 'X') {
             playerStatus.textContent = `${player1Name} Won!`;
+            winningRecord[player1Name] += 1;
+            updateScores();
             gameStatus = false;
+            flag = 1;
             return;
         } else if (cells[idx1].textContent === '0' && cells[idx2].textContent === '0' && cells[idx3].textContent === '0'){
             playerStatus.textContent = `${player2Name} Won!`;
+            winningRecord[player2Name] += 1;
+            updateScores();
             gameStatus = false;
+            flag = 1;
             return;
         }
-        else if(cells[0].textContent !== '' && cells[1].textContent !== '' && cells[2].textContent !== '' && cells[3].textContent !== '' && cells[4].textContent !== '' && cells[5].textContent !== '' &&cells[6].textContent !== '' && cells[7].textContent !== '' && cells[8].textContent !== '' ){
+        else if(cells[0].textContent !== '' && cells[1].textContent !== '' && cells[2].textContent !== '' && cells[3].textContent !== '' && cells[4].textContent !== '' && cells[5].textContent !== '' &&cells[6].textContent !== '' && cells[7].textContent !== '' && cells[8].textContent !== '' && !flag){
             playerStatus.textContent = `You have a DRAW!`;
+            console.log('you have a draw!');
         }
     }
+
+// function drawCase() {
+//     let cells = document.querySelectorAll(`#app .cell`);
+//     for(let idx = 0; idx < cells.length; idx++) {
+//         if(cells[idx].textContent !== '' && !flag) {
+//             playerStatus.textContent = `You have a DRAW!`;
+//         }
+//     }
+// }
+
 function winner() {
     let cells = document.querySelectorAll(`#app .cell`);
    checkCase(0, 1, 2)
@@ -72,11 +112,16 @@ function winner() {
    checkCase(6, 7, 8);
    checkCase(0, 3, 6);
    checkCase(1, 4, 7);
+   checkCase(2, 5, 8);
    checkCase(0, 4, 8);
    checkCase(2, 4, 6);
 }
 
 
 document.getElementById(`restart-btn`).addEventListener(`click`, ()=> {
-    window.location.reload();
+    let cells = document.querySelectorAll(`#app .cell`);
+        for(let idx = 0; idx < cells.length; idx++) {
+            cells[idx].textContent = '';
+            gameStatus = true;
+        }
 });
